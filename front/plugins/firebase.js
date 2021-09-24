43b1c201e59b52419d5b3a4881668c06a58e7d8d
tom1236908745 <tom1233908745@gmail.com>
2021-09-24 17:20:59 +0900

    add env

--- front/plugins/firebase.js
+++ front/plugins/firebase.js
@@ -1,15 +1,27 @@
 import firebase from 'firebase/compat/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
-
+// ** Firebaseプロジェクトの設定を記す
+/* if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: $config.APIKEY,
+    authDomain: $config.AUTHDOMAIN,
+    databaseURL: $config.DAABASEURL,
+    projectId: $config.PROJECTID,
+    storageBucket: $config.STORAGEBUCKET,
+    messagingSenderId: $config.MESSAGINGSENDERID// ** cloudmessagingを使う場合は設定
+  })
+} */
+console.log("process.env.APIKEY",process.env.APIKEY);
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
     apiKey: process.env.APIKEY,
     authDomain: process.env.AUTHDOMAIN,
-    databaseURL: process.env.DAABASEURL,
     projectId: process.env.PROJECTID,
     storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: "G-4F06MB34BE",
   });
 }
 
