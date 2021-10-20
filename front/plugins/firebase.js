30d64782f37e85925dbad5ebc20ad7684e76b4cf
tom1236908745 <tom1233908745@gmail.com>
2021-10-20 21:12:03 +0900

    firebase一部修正

--- front/plugins/firebase.js
+++ front/plugins/firebase.js
@@ -19,20 +19,21 @@ import {
   where
 } from 'firebase/firestore'
 
-
+let firebaseApp = {}
 
 if (!firebase.apps.length) {
-  let firebaseApp = firebase.initializeApp({
+  firebaseApp = firebase.initializeApp({
     apiKey: process.env.APIKEY,
     authDomain: process.env.AUTHDOMAIN,
     projectId: process.env.PROJECTID,
     storageBucket: process.env.STORAGEBUCKET,
     messagingSenderId: process.env.MESSAGINGSENDERID,
     appId: process.env.APPID,
-    measurementId: process.env.MEASUREMENTID,
+    measurementId: 'G-4F06MB34BE',
   })
+} else {
+  firebaseApp = apps[0]
 }
-
 const db = getFirestore(firebaseApp, {})
 export {
   firebase,
